1f9d720f62e1a6197343632e52aaf1162ce805c7
Deve <80102780+pro-fish@users.noreply.github.com>
2024-12-07 17:47:29 +0300

    Update traffic_patterns.py

--- traffic_patterns.py
+++ traffic_patterns.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-def define_traffic_patterns(city_name="Jeddah"):
+def define_traffic_patterns(city_name="Makkah Region"):
     city_data_dir = Path("data") / city_name
     city_data_dir.mkdir(parents=True, exist_ok=True)
     
@@ -20,3 +20,6 @@ def define_traffic_patterns(city_name="Jeddah"):
     with open(city_data_dir / "traffic_patterns.json", "w") as f:
         json.dump(traffic_patterns, f, indent=4)
     print(f"Traffic patterns defined for {city_name}.")
+
+if __name__ == "__main__":
+    define_traffic_patterns()
